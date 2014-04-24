7238d902c451ed73946c9c91a56a4314cec5cd70
Markus Schwarz <post@markus-schwarz.net>
2014-04-24 22:02:18 +0200

    determines locale on each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  def set_locale
+    custom_locale = params[:locale] || extract_locale_from_accept_language_header
+    I18n.locale = custom_locale || I18n.default_locale
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
