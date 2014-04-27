872267a9b4b7f1f1fd1d45820cb1f3ba659c7253
Markus Schwarz <post@markus-schwarz.net>
2014-04-27 22:25:42 +0200

    changes fb to display: 'popup'

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], popup: true
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], display: 'popup'
 end
