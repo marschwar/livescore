ca23511a2be2909ab6141b8c4363b22381074caf
Markus Schwarz <post@markus-schwarz.net>
2014-05-07 22:50:13 +0200

    adds title and alt attribute to team image

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,6 @@
 module TeamsHelper
 
   def team_image(team, size = 50)
-    raw(%Q(<img src="#{team.encoded_image}" style="max-width: #{size}px;max-height: #{size}px" />)) if team.encoded_image.present?
+    raw(%Q(<img src="#{team.encoded_image}" alt="#{team.name} Logo" title="#{team.name} Logo" style="max-width: #{size}px;max-height: #{size}px" />)) if team.encoded_image.present?
   end
 end
