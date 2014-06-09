ad56b6b5feab4a4101bb2864440b79c3ad8ab41d
Markus Schwarz <post@markus-schwarz.net>
2014-06-09 17:38:16 +0200

    does not abbreviate U<nn>

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,16 @@ class Team < ActiveRecord::Base
   belongs_to :user
 
   def abbreviated_name
-    name.split.map { |s| s[0] }.join
+  	if /(?<team_name>.*)\s(?<suffix>U\d+)\s*$/i =~ name
+	    short_name = team_name.split.map { |s| s[0] }.join
+	    "#{short_name} #{suffix.upcase}"
+	  else
+	    name.split.map { |s| s[0] }.join
+		end
   end
+
+private
+	def age_suffix?
+		/.*([Uu]\d+)\s*$/.match name
+	end
 end
