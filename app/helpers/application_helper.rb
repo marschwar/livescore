064ab5775f6ce01dbcb9d64ba1e8fd8371d2bd21
Markus Schwarz <post@markus-schwarz.net>
2014-07-13 15:30:01 +0200

    adds title and alt tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
-  def entity_image(entity, options = { size: 50 })
+  def entity_image(entity, options = { })
     if entity.encoded_image.present?
-      options[:style] = "max-width: #{options[:size]}px;max-height: #{options[:size]}px" 
+      options = { size: 50, title: entity.name, alt: entity.name }.merge(options)
+      options[:style] = "#{options[:style]};max-width: #{options[:size]}px;max-height: #{options[:size]}px" 
       image_tag "#{polymorphic_path(entity)}.#{entity.image_type}", options 
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
 
   include HasImage
 
+  alias_attribute :name, :common_name
+
   scope :is_like, -> (name) { where("upper(common_name) like ?", "%#{name.upcase}%")}
 end
