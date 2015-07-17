f15e43671900018bc5408000da4c5ad7eede1f4c
Markus Schwarz <post@markus-schwarz.net>
2015-07-17 10:35:49 +0200

    changes upload folder to share environments

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,7 +13,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{Rails.env}/avatar/#{model.id}"
+    Rails.env.production? ? "avatar/#{model.id}" : "#{Rails.env}/avatar/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
